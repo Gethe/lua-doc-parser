cf93c97c067599bfbd693caa0021716ce3ebecdc
Gethe <gethe88@gmail.com>
2020-09-26 16:33:03 -0500

    Use https instead of ssh
    Do https push
    
    Fix url

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-if [[ -z "$CI" ]]; then
+if [ -z "$CI" ]; then
 	echo "No CI"
 	exit 1
 fi
 
-if [[ -n "$CIRCLECI" ]]; then
+if [ -n "$CIRCLECI" ]; then
 	COMMIT_SHA="$CIRCLE_SHA1"
 	REPO_PATH="$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME"
 fi
-if [[ -n "$GITHUB_ACTION" ]]; then
+if [ -n "$GITHUB_ACTION" ]; then
 	COMMIT_SHA="$GITHUB_SHA"
 	REPO_PATH="$GITHUB_REPOSITORY"
 fi
@@ -19,19 +19,21 @@ git config --global user.email "<>"
 git config --global user.name "${GIT_NAME:-CI}"
 
 # clone the wiki repo
-git clone "git@github.com:$REPO_PATH.wiki" .wiki
+git clone "https://github.com/$REPO_PATH.wiki" .wiki
 
 # run the python script
 parse -o ".wiki" || exit 1
 
 # navigate to the output directory
-cd .wiki
+cd .wiki || exit 1
 
-# stage changes
-git add .
+if [ -n "$CI" ]; then
+	# stage changes
+	git add .
 
-# commit using the last commit SHA as the message
-git commit -m "$COMMIT_SHA"
+	# commit using the last commit SHA as the message
+	git commit -m "$COMMIT_SHA"
 
-# push the wiki repo
-git push origin master
+	# push the wiki repo
+	git push "https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${REPO_PATH}.wiki" master
+fi
